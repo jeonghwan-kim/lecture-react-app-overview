60e0970785f47282dd27595943900dc62d300010
김정환 <ej88ej@gmail.com>
2018-06-20 13:54:51 +0900

    라우팅

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Topics from './Topics';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom';
-import './index.css';
-
 import App from './components/App'
+
 
 ReactDom.render(
   <App />,
