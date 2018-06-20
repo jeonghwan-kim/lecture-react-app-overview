08cd4b462fa2f8bd9327569357ee8034999c6684
김정환 <ej88ej@gmail.com>
2018-06-20 13:39:27 +0900

    불필요 코드 제거

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+
+ReactDom.render(
+  <App />,
+  document.querySelector('#root')
+)
